1c699f8cfd162d3c7cc614ce96afafbce8a3e9c8
Jonathan Hernandez / jonathan17ux <87735560+jonathan17ux@users.noreply.github.com>
2025-06-30 00:01:13 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -30,7 +30,7 @@ canvas.create_image(0, 0, anchor=tk.NW, image=bg_photo)
 
 # === Time / Date Text ===
 time_text = canvas.create_text(
-    325, 130, fill="#f4bf18", font=("Impact", 105), anchor="e")
+    325, 130, fill="#f4bf18", font=("Impact", 95), anchor="e")
 p_text = canvas.create_text(
     390, 145, fill="#f4bf18", font=("Impact", 45))
 date_text = canvas.create_text(
